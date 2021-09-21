4a65178e8f859f8422733e39f6a976a3098691e1
Kate Bryan <kate.bryan@and.digital>
2021-09-21 13:56:55 +0100

    fix: updated middleware config file to bring in env variables

--- test-store/middleware.config.js
+++ test-store/middleware.config.js
@@ -1,15 +1,21 @@
+const projectKey = process.env.VUE_APP_CTP_PROJECT_KEY;
+const clientSecret = process.env.VUE_APP_CTP_CLIENT_SECRET;
+const clientId = process.env.VUE_APP_CTP_CLIENT_ID;
+const uri = process.env.VUE_APP_CTP_API_URL;
+const scopes = process.env.VUE_APP_CTP_SCOPES;
+
 module.exports = {
   integrations: {
     ct: {
       location: "@vue-storefront/commercetools-api/server",
       configuration: {
         api: {
-          uri: `${process.env.VUE_APP_CTP_API_URL}/vsf-ct-dev/graphql`,
+          uri: `${uri}/${projectKey}/graphql`,
           authHost: "https://auth.sphere.io",
-          projectKey: `${process.env.VUE_APP_CTP_PROJECT_KEY}`,
-          clientId: `${process.env.VUE_APP_CTP_CLIENT_ID}`,
-          clientSecret: `${process.env.VUE_APP_CTP_CLIENT_SECRET}`,
-          scopes: [`${process.env.VUE_APP_CTP_SCOPES}`]
+          projectKey,
+          clientId,
+          clientSecret,
+          scopes: [scopes]
         },
         currency: "USD",
         country: "US"
